6b01a2237d0f5e76bd200a24aef167b8e4da782e
Apoorva Kashinath <apoorva.kashinath@atcs.com>
2023-01-13 14:45:30 +0530

    pictures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortonaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+    return(
+    <div>
+        <section className='hero is-primary'>
+            <div className='hero-body'>
+              <p className='title'>Personal Digital Assistance </p>
+            </div>
+        </section>
+        <div className='container'>
+            <section className='section'>
+                <div className='columns'>
+                    <div className='column is-4'>
+                      <ProfileCard 
+                      title='Alexa'
+                      handle='alexa99'
+                      image={AlexaImage}
+                      description="Alexa was created by Amazon"
+                         />
+                    </div>
+                    <div className='column is-4'>
+                      <ProfileCard
+                       title='cortona'
+                       handle='cortona88' 
+                       image={CortonaImage} 
+                       description="Cortona was created by Microsoft"/>
+                    </div>
+                    <div className='column is-4'>
+                      <ProfileCard 
+                      title='siri' 
+                      handle='siri77' 
+                      image={SiriImage}
+                      description='Siri was created by Apple'
+                      />
+                    </div>
+                </div>
+            </section>
+        </div>
+
     </div>
-  );
+    );
 }
 
 export default App;
